9e83d97d5a11c2235a121805ecbb8aa99217f741
Attila Simon <sati290@gmail.com>
2019-11-05 21:43:24 +0100

    strava auth: send user to auth page

--- packages/website/src/routes/Home.tsx
+++ packages/website/src/routes/Home.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+
+const authorizeParams = {
+  client_id: process.env.REACT_APP_STRAVA_CLIENT_ID,
+  redirect_uri: window.location.origin + '/authCallback',
+  response_type: 'code',
+  scope: process.env.REACT_APP_STRAVA_SCOPES,
+};
+const authorizeQueryString = Object.entries(authorizeParams)
+  .map(([key, value]) => `${key}=${encodeURIComponent(value!)}`)
+  .join('&');
+const authorizeUrl =
+  'https://www.strava.com/oauth/authorize?' + authorizeQueryString;
 
 type HomeProps = {
   user: firebase.User | any;
 };
 
 export default function Home({ user }: HomeProps) {
-  return <Typography>Hello {user.email}!</Typography>;
+  return (
+    <>
+      <Typography>Hello {user.email}!</Typography>
+      <Button href={authorizeUrl}>Authorize with strava</Button>
+    </>
+  );
 }
