e697e93a79117556e5c78359b32afb841acc8fce
Attila Simon <sati290@gmail.com>
2019-11-05 23:07:54 +0100

    strava auth: cloud function for token exchange

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,8 +1,4 @@
 import * as functions from 'firebase-functions';
+import stravaAuthorizeApp from './strava/authorize';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const stravaAuthorize = functions.https.onRequest(stravaAuthorizeApp);
---
+++ packages/functions/src/strava/authorize.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import axios from 'axios';
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const { code, scope } = req.body;
+
+  if (!code || !scope) {
+    res.sendStatus(400);
+  }
+
+  console.log('authorize code:', code, 'scope:', scope);
+  axios
+    .post('https://www.strava.com/oauth/token', {
+      client_id: 40447,
+      client_secret: '',
+      code,
+      grant_type: 'authorization_code',
+    })
+    .then(response => {
+      console.log(response);
+      res.sendStatus(200);
+    })
+    .catch(error => {
+      console.log('error:', error);
+      res.sendStatus(500);
+    });
+});
+
+export default app;
