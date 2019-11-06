891028219e2faa672e1c8414e3fba6f265d99718
Attila Simon <sati290@gmail.com>
2019-11-06 13:54:35 +0100

    initialize firebase sdk before use in functions

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as express from 'express';
 import stravaApp from './strava';
+
+admin.initializeApp();
 
 const app = express();
 
