c380eccf83ff6d707c58504cf588da90796fd01a
Attila Simon <sati290@gmail.com>
2019-11-06 20:35:21 +0100

    redirect from auth callback page on success

--- packages/website/src/routes/StravaAuthCallback.tsx
+++ packages/website/src/routes/StravaAuthCallback.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router';
-import { Typography } from '@material-ui/core';
+import { useLocation, useHistory } from 'react-router';
+import { Typography, Container } from '@material-ui/core';
 import * as firebase from 'firebase/app';
 import * as qs from 'querystring';
 import axios from 'axios';
 
 const AuthCallback: React.FC = () => {
   const location = useLocation();
+  const history = useHistory();
   const query = qs.parse(location.search);
 
   useEffect(() => {
@@ -15,29 +16,29 @@ const AuthCallback: React.FC = () => {
     firebase
       .auth()
       .currentUser!.getIdToken(true)
-      .then(token => {
-        axios
-          .post(
-            (process.env.REACT_APP_API_ORIGIN || '') +
-              '/api/providers/strava/link',
-            {
-              code,
-              scope,
-            },
-            { headers: { Authorization: 'Bearer ' + token } }
-          )
-          .then(response => {
-            console.log(response);
-          })
-          .catch(error => {
-            console.log(error);
-          });
+      .then(token =>
+        axios.post(
+          (process.env.REACT_APP_API_ORIGIN || '') +
+            '/api/providers/strava/link',
+          {
+            code,
+            scope,
+          },
+          { headers: { Authorization: 'Bearer ' + token } }
+        )
+      )
+      .then(response => {
+        console.log(response);
+        history.replace('/');
       })
-      .catch(error => console.log('token error:', error));
+      .catch(console.error);
   });
 
-  //return <Redirect to="/" />;
-  return <Typography>Authorizing...</Typography>;
+  return (
+    <Container>
+      <Typography>Authorizing...</Typography>
+    </Container>
+  );
 };
 
 export default AuthCallback;
