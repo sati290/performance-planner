fcfe5b9d7ae4f4959e82799388fe95d13c698252
Attila Simon <sati290@gmail.com>
2019-11-06 11:50:21 +0100

    strava auth: call api endpoint from auth callback

--- packages/website/src/routes/AuthCallback.tsx
+++ packages/website/src/routes/AuthCallback.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router';
 import { Typography } from '@material-ui/core';
 import * as qs from 'querystring';
+import axios from 'axios';
 
 const AuthCallback: React.FC = () => {
   const location = useLocation();
@@ -14,6 +15,18 @@ const AuthCallback: React.FC = () => {
       'scope:',
       query.scope
     );
+
+    axios
+      .post(process.env.REACT_APP_API_ORIGIN + '/api/strava/authorize', {
+        code: query.code,
+        scope: query.scope,
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   });
 
   //return <Redirect to="/" />;
