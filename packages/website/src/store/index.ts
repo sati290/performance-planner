56a406d187c119dab004832f878380b6f71202bc
Attila Simon <sati290@gmail.com>
2019-11-10 10:25:32 +0100

    remove redux logger, user npm module for redux devtools

--- packages/website/src/store/index.ts
+++ packages/website/src/store/index.ts
@@ -1,18 +1,11 @@
-import { createStore, applyMiddleware, compose, Middleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-let middleware: Array<Middleware> = [thunkMiddleware];
-if (process.env.NODE_ENV !== 'production') {
-  middleware = [createLogger(), ...middleware];
-}
-
-const composeEnhancers =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(thunkMiddleware))
 );
 
 export default store;
